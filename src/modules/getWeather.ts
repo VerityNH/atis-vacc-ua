a3380b7fcd2de1763611060632a6f0e71d597c2e
Mykola Iankovskyi <mykola.iankovskyi@avid.com>
2023-11-01 12:08:56 -0400

    Use https://www.weatherapi.com/ as a weather source

--- src/modules/getWeather.ts
+++ src/modules/getWeather.ts
@@ -3,29 +3,45 @@ import { Airport, Weather } from 'src/types';
 
 export const getWeather = async (airport: Airport): Promise<Weather> => {
   const key = process.env.WEATHER_KEY;
-  const url = `https://api.openweathermap.org/data/3.0/onecall?lon=${airport.lon}&lat=${airport.lat}&units=metric&exclude=minutely,hourly,daily,alerts&appid=${key}`;
+  //const url = `https://api.openweathermap.org/data/3.0/onecall?lon=${airport.lon}&lat=${airport.lat}&units=metric&exclude=minutely,hourly,daily,alerts&appid=${key}`;
+  const url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${airport.lat},${airport.lon}&aqi=no`;
   return axios.get(url).then((res) => {
+    const calcDP = (temp, humi) => {
+      return (
+        temp -
+        (14.55 + 0.114 * temp) * (1 - 0.01 * humi) -
+        Math.pow((2.5 + 0.007 * temp) * (1 - 0.01 * humi), 3) -
+        (15.9 + 0.117 * temp) * Math.pow(1 - 0.01 * humi, 14)
+      );
+    };
+
+    const qnh = Math.round(res.data.current.pressure_mb);
+    const temperature = Math.round(res.data.current.temp_c);
+    const humidity = res.data.current.humidity;
+    const dewPoint = Math.round(calcDP(temperature, humidity));
+    const visibility = Math.round(res.data.current.vis_km);
+
     return {
       wind: [
-        res.data.current.wind_deg,
-        Math.round(res.data.current.wind_speed),
+        res.data.current.wind_degree,
+        Math.round((res.data.current.wind_kph * 5) / 18),
       ],
-      qnh: res.data.current.pressure,
-      temperature: Math.round(res.data.current.temp),
-      dewPoint: Math.round(res.data.current.dew_point),
+      qnh: qnh,
+      temperature: temperature,
+      dewPoint: dewPoint,
       tl:
-        res.data.current.pressure >= 1031
+        qnh >= 1031
           ? 105
-          : res.data.current.pressure >= 1013
+          : qnh >= 1013
           ? 110
-          : res.data.current.pressure >= 995
+          : qnh >= 995
           ? 115
-          : res.data.current.pressure >= 977
+          : qnh >= 977
           ? 120
-          : res.data.current.pressure >= 959
+          : qnh >= 959
           ? 125
           : 130,
-      visibility: Math.round(res.data.current.visibility / 1000),
+      visibility: visibility,
     };
   });
 };
